a389771ae7116ea2c5d614badff3d560e16c3e5b
Aswin v gopal <aswinvazhathodiyil113@gmail.com>
2025-01-23 13:48:19 +0530

    refresh token logic implemented

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from accounts.views import RegisterView, CustomLoginView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
