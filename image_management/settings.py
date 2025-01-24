b277158690093767219a94b9cc96f75e9c50dffd
Aswin v gopal <aswinvazhathodiyil113@gmail.com>
2025-01-24 23:29:44 +0530

    changed allowed host to redners

--- image_management/settings.py
+++ image_management/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-j@hy7=0sbstj1k@jt5fb=hvoxanf#4n4bvu@f@51fx*@7@t91#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['imagemanagement-zy2e.onrender.com']
 
 
 # Application definition
