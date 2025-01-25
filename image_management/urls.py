0f8b695443d0a4b0f73e60246b3c0550ed34d5c0
Aswin v gopal <aswinvazhathodiyil113@gmail.com>
2025-01-25 20:53:32 +0530

    urlspatterns for the image is updated

--- image_management/urls.py
+++ image_management/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),  # Use `include` here
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
