df18f895c32de5870b9a57f5cf50a7235e0f4a47
Aswin v gopal <aswinvazhathodiyil113@gmail.com>
2025-03-18 18:37:38 +0530

    changed the setIsUploadPopupOpen

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
+import uuid
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
@@ -23,6 +24,7 @@ class UserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 class User(AbstractBaseUser, PermissionsMixin):
+    user_id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
     email = models.EmailField(unique=True)
     full_name = models.CharField(max_length=100)
     phone = models.CharField(max_length=15, null=True, blank=True)
